ea0da25fbbb076c5f601c8d0d4faea4b2b1c3807
Shahram032 <Shahram.Abaie@Gmail.com>
2022-06-14 18:26:52 +0430

    Work

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,18 +1,38 @@
 import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
+import { TablerIconsModule } from 'angular-tabler-icons';
+import {
+  IconCamera,
+  IconHeart,
+  IconBrandGithub,
+  IconEdit,
+  IconCirclePlus,
+  IconHandStop,
+  IconLock,
+  IconKey,
+} from 'angular-tabler-icons/icons';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { HomeComponent } from './components/home/home.component';
+import { LoginComponent } from './components/login/login.component';
+import { UserComponent } from './components/user/user.component';
+
+const icons = {
+  IconCamera,
+  IconHeart,
+  IconBrandGithub,
+  IconEdit,
+  IconCirclePlus,
+  IconHandStop,
+  IconLock,
+  IconKey
+};
 
 @NgModule({
-  declarations: [
-    AppComponent
-  ],
-  imports: [
-    BrowserModule,
-    AppRoutingModule
-  ],
+  declarations: [AppComponent, HomeComponent, LoginComponent, UserComponent],
+  imports: [BrowserModule, AppRoutingModule, TablerIconsModule.pick(icons)],
   providers: [],
-  bootstrap: [AppComponent]
+  bootstrap: [AppComponent],
 })
-export class AppModule { }
+export class AppModule {}
