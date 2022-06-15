10e8b3b7778d6864ac6bca5afceb3771d835ac49
Shahram032 <Shahram.Abaie@Gmail.com>
2022-06-15 18:09:34 +0430

    Work

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -2,13 +2,23 @@ import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
 import { HomeComponent } from './components/home/home.component';
 import { LoginComponent } from './components/login/login.component';
+import { ChartComponent } from './components/org/chart/chart.component';
+import { InfoComponent } from './components/org/info/info.component';
+import { RoleAccessComponent } from './components/role/role-access/role-access.component';
+import { RoleComponent } from './components/role/role/role.component';
 import { UserComponent } from './components/user/user.component';
+import { WorkFlowComponent } from './components/work-flow/work-flow/work-flow.component';
 
 const routes: Routes = [
   { path: '', pathMatch: 'full', redirectTo: 'home' },
   { path: 'home', component: HomeComponent },
   { path: 'login', component: LoginComponent },
-  { path: 'user', component: UserComponent }
+  { path: 'user', component: UserComponent },
+  { path: 'work_flow', component: WorkFlowComponent },
+  { path: 'role', component: RoleComponent },
+  { path: 'role_access', component: RoleAccessComponent },
+  { path: 'org_chart', component: ChartComponent },
+  { path: 'org_info', component: InfoComponent }
 ];
 
 @NgModule({
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -19,6 +19,11 @@ import { HomeComponent } from './components/home/home.component';
 import { LoginComponent } from './components/login/login.component';
 import { UserComponent } from './components/user/user.component';
 import { AccessService } from './service/access.service';
+import { WorkFlowComponent } from './components/work-flow/work-flow/work-flow.component';
+import { RoleComponent } from './components/role/role/role.component';
+import { RoleAccessComponent } from './components/role/role-access/role-access.component';
+import { ChartComponent } from './components/org/chart/chart.component';
+import { InfoComponent } from './components/org/info/info.component';
 
 const icons = {
   IconCamera,
@@ -33,7 +38,7 @@ const icons = {
 };
 
 @NgModule({
-  declarations: [AppComponent, HomeComponent, LoginComponent, UserComponent],
+  declarations: [AppComponent, HomeComponent, LoginComponent, UserComponent, WorkFlowComponent, RoleComponent, RoleAccessComponent, ChartComponent, InfoComponent],
   imports: [BrowserModule, AppRoutingModule, TablerIconsModule.pick(icons)],
   providers: [AccessService],
   bootstrap: [AppComponent],
